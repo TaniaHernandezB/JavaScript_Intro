fc19e7e76e27fbb5313552f1fbc402b21dda0d1f
Tania Hernandez <taniahernandez@Tanias-MacBook-Air.local>
2022-07-05 19:02:33 -0400

    veryeasy

---
+++ VeryEasy/veasy.js
@@ -0,0 +1,6 @@
+// VERY EASY: Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
+
+var num1 = 6;
+var num2=2;
+
+console.log(`The difference between ${num1} and ${num2} is ${num1-num2}`)
